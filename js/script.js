46f51b9317072f61da41eee7803cb959f9d546d4
MicheliniDev <michelinidev@gmail.com>
2024-04-18 12:12:29 -0300

    Adicionando arquivos

---
+++ js/script.js
@@ -0,0 +1,7 @@
+let canvasPong = document.getElementById("canvas")        
+let quadro = canvasPong.getContext("2d")
+let player1 = quadro.fillRect(100, 100, 50, 50)
+
+/*player1.addEventListener("keydown", {key : "W"}, function() {
+    player1.
+})*/
