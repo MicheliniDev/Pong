676a0ecfe41df9a16479c576bb6797d4c591ef9d
MicheliniDev <michelinidev@gmail.com>
2024-05-02 12:18:45 -0300

    Adicionando mais arquivos

--- js/script.js
+++ js/script.js
@@ -1,7 +1,37 @@
 let canvasPong = document.getElementById("canvas")        
 let quadro = canvasPong.getContext("2d")
-let player1 = quadro.fillRect(100, 100, 50, 50)
 
-/*player1.addEventListener("keydown", {key : "W"}, function() {
-    player1.
-})*/
+quadro.fillStyle = "white" // permite preencher a cor dos objetos dentro do quadro
+let player1 = {
+    px: 80,
+    py: 260,
+    tx: 30,
+    ty: 200,
+}
+let player2 = {
+    px: 1150,
+    py: 260,
+    tx: 30,
+    ty: 200,
+}
+let bolinha = {
+    px: 625,
+    py: 345,
+    tx: 30,
+    ty: 30,
+}
+
+
+quadro.font = "20px Comic Sans MS"
+let pts1 = 0
+let pts2 = 0
+
+function draw() {
+    quadro.fillRect(player1.px, player1.py, player1.tx, player1.ty)
+    quadro.fillRect(player2.px, player2.py, player2.tx, player2.ty)
+    quadro.fillRect(bolinha.px, bolinha.py, bolinha.tx, bolinha.ty)
+    quadro.fillText(`Pontos: ${pts1}`, 200, 70)
+    quadro.fillText(`Pontos: ${pts2}`, 1000, 70)
+}
+
+draw()
