872dbab61f7a6f36716bebd2c45221815caa622a
MicheliniDev <michelinidev@gmail.com>
2024-05-23 12:07:16 -0300

    Adicionei varias bostas

--- js/script.js
+++ js/script.js
@@ -7,12 +7,14 @@ let player1 = {
     py: 260,
     tx: 30,
     ty: 200,
+    dir : 0
 }
 let player2 = {
     px: 1150,
     py: 260,
     tx: 30,
     ty: 200,
+    dir: 0
 }
 let bolinha = {
     px: 625,
@@ -21,7 +23,6 @@ let bolinha = {
     ty: 30,
     dir: 8,
 }
-
 
 quadro.font = "20px Comic Sans MS"
 let pts1 = 0
@@ -35,9 +36,58 @@ function draw() {
     quadro.fillText(`Pontos: ${pts2}`, 900, 70)
 }
 
-document.addEventListener("keydown", function(e){
-    player1.py -= 10
+document.addEventListener("keydown", function(keyPressed){
+    if(keyPressed.keyCode == 87) {
+        player1.dir -= 8
+    }
+    else if(keyPressed.keyCode == 83){
+        player1.dir += 8
+    }
 })
+
+document.addEventListener("keyup", function(keyPressed) {
+    if(keyPressed.keyCode == 87) {
+        player1.dir = 0
+    }
+    else if(keyPressed.keyCode == 83){
+        player1.dir = 0
+    }
+})
+
+document.addEventListener("keydown", function(keyPressed){
+    if(keyPressed.keyCode == 38) {
+        player2.dir -= 8
+    }
+    else if(keyPressed.keyCode == 40){
+        player2.dir += 8
+    }
+})
+
+document.addEventListener("keyup", function(keyPressed){
+    if(keyPressed.keyCode == 38) {
+        player2.dir = 0
+    }
+    else if(keyPressed.keyCode == 40){
+        player2.dir = 0
+    }
+})
+
+function moverJogador() {
+    if (player1.py < 0) {
+        player1.py = 0
+    }
+    if (player1.py > 520) {
+        player1.py = 520
+    }
+    if (player2.py < 0) {
+        player2.py = 0
+    }
+    if (player2.py > 520) {
+        player2.py = 520
+    }
+    player1.py += player1.dir
+    player2.py += player2.dir
+}
 
 function moverBolinha() {
     bolinha.px += bolinha.dir
@@ -49,11 +99,22 @@ function moverBolinha() {
     }
 }
 
+function placar() {
+    if(bolinha.px < 90) {
+        pts1 += 1 
+    }
+    else if (bolinha.px > 1150) {
+        pts2 += 1
+    }
+}
+
 function main() {
     quadro.clearRect(0, 0, 1280, 720) // wow limpa as coisas
 
     draw()
     moverBolinha()
+    placar()
+    moverJogador()
 }
 
 setInterval(main, 10) 
